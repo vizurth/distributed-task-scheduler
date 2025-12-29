a20fc8a5767c89ddc10a402bac1fbebe8f4e8fb1
vizurth <81080666+vizurth@users.noreply.github.com>
2025-12-29 15:06:56 +0300

    feat: add not impl handler method

--- internal/api/handler/handler.go
+++ internal/api/handler/handler.go
@@ -1,8 +1,13 @@
 package handler
 
 import (
+	"context"
+
 	"github.com/vizurth/distributed-task-scheduler/internal/api/service"
 	taskpb "gitlab.com/vizurth/protos/gen/go/task/task-api-service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type handlerImpl struct {
@@ -15,4 +20,17 @@ func NewHandler(service service.Service) Handler {
 	return &handlerImpl{
 		service: service,
 	}
+}
+
+func (h *handlerImpl) SubmitTask(context.Context, *taskpb.SubmitTaskRequest) (*taskpb.SubmitTaskResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "method SubmitTask not implemented")
+}
+func (h *handlerImpl) GetTaskStatus(context.Context, *taskpb.GetTaskStatusRequest) (*taskpb.GetTaskStatusResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "method GetTaskStatus not implemented")
+}
+func (h *handlerImpl) CancelTask(context.Context, *taskpb.CancelTaskRequest) (*taskpb.CancelTaskResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "method CancelTask not implemented")
+}
+func (h *handlerImpl) ListTasks(*taskpb.ListTasksRequest, grpc.ServerStreamingServer[taskpb.TaskInfo]) error {
+	return status.Error(codes.Unimplemented, "method ListTasks not implemented")
 }
